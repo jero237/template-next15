45042cde3433112ddc86e942ec0a33859283d3bb
Jeronimo Balestra <jeronimobalestra@gmail.com>
2025-04-22 11:08:50 -0300

    Update database connection details and remove root environment path in generated Prisma files

--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -209,7 +209,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
@@ -219,12 +219,11 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": null
+        "value": "postgresql://neondb_owner:npg_qp2PujXTbog6@ep-spring-hat-a4vbipwo-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require"
       }
     }
   },
